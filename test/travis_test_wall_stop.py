10f248cb1da1d3782350d4012f64b893dde211cb
nzhoo <nzh_star@zzzstar.com>
2018-01-25 15:41:12 +0000

    Change travis_test_wall_stop file

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -21,13 +21,13 @@ class WallStopTest(unittest.TestCase):
         return left,right
 
     def test_io(self):
-        self.set_and_get(400,100,100,0) #total: 600
+        left, right = self.set_and_get(400,100,100,0) #total: 600
         self.assertTrue(left == 0 and right == 0,"can't stop")
 
-        self.set_and_get(400,0,0,99)  #total: 499
+        left, right = self.set_and_get(400,0,0,99)  #total: 499
         self.assertTrue(left != 0 and right != 0,"can't move again")
 
-        self.set_and_get(150,0,200,150)  #total: 500
+        left, right = self.set_and_get(150,0,200,150)  #total: 500
         self.assertTrue(left != 0 and right != 0,"can't stop")
 
 if __name__ == '__main__':

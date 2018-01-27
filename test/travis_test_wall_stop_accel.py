7bc3f5ce0b876a0302b07eeb16c304ad483faa1f
nzhoo <nzh_star@zzzstar.com>
2018-01-27 11:27:03 +0000

    Change travis_test_wall_stop_accel.py

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -29,10 +29,10 @@ class WallStopAccelTest(unittest.TestCase):
 
         time.sleep(5.0)
         left, right = self.set_and_get(40,0,0,9)  #total: 49
-        self.assertTrue(2000 < left == right == 0,"can't accel")
+        self.assertTrue(2000 < left == right,"can't accel")
 
         left, right = self.set_and_get(15,0,20,15)  #total: 50
-        self.assertTrue(left == right == 0,"can't stop")
+        self.assertTrue(left == right == 0,"can't stop again")
 
 if __name__ == '__main__':
     time.sleep(3)
